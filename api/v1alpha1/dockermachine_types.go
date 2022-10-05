c3f23226d72867dd9f9d2f6c85a4d5587f481338
Anusha Hegde <anushah@vmware.com>
2022-10-05 14:09:39 +0000

    Add required fields in Machine API
    Per the CAPI Machine Infrastructure contract, added the required fields.
    Auto-generated CRD and deepcopy files

--- api/v1alpha1/dockermachine_types.go
+++ api/v1alpha1/dockermachine_types.go
@@ -18,6 +18,7 @@ package v1alpha1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
 )
 
 // EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
@@ -28,14 +29,22 @@ type DockerMachineSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of DockerMachine. Edit dockermachine_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// ProviderID is the identifier for the DockerMachine instance
+	ProviderID string `json:"providerID,omitempty"`
 }
 
 // DockerMachineStatus defines the observed state of DockerMachine
 type DockerMachineStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+
+	// Ready indicates the docker infrastructure has been provisioned and is ready
+	// +optional
+	Ready bool `json:"ready"`
+
+	// Conditions defines current service state of the BYOMachine.
+	// +optional
+	Conditions clusterv1.Conditions `json:"conditions,omitempty"`
 }
 
 //+kubebuilder:object:root=true
