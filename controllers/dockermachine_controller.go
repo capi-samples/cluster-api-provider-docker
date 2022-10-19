0312deb2493a173b8a0eb1dedf1d35d96b22e404
Winnie Kwon <pydctw@gmail.com>
2022-10-18 20:56:21 -0700

    Add an additional rbac role to the DockerMachine controller

--- controllers/dockermachine_controller.go
+++ controllers/dockermachine_controller.go
@@ -54,6 +54,7 @@ type DockerMachineReconciler struct {
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=dockermachines,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=dockermachines/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=infrastructure.cluster.x-k8s.io,resources=dockermachines/finalizers,verbs=update
+//+kubebuilder:rbac:groups=cluster.x-k8s.io,resources=machines;machines/status,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
